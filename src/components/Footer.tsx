db6463c2c082cc1e0b3335aa6ba7c538931a4759
Arya <ngurahpratama2002@gmail.com>
2025-01-08 10:31:26 +0700

    Done adding maps on Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,51 +1,7 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L, { LatLngExpression } from "leaflet";
-
-const redMarker = L.icon({
-  iconUrl:
-    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
-
-// Custom style untuk popup dengan keterbacaan yang lebih baik
-const customPopupStyle = `
-  .custom-popup .leaflet-popup-content-wrapper {
-    background: #FFFFFF;
-    border-radius: 8px;
-    box-shadow: 0 2px 10px rgba(0,0,0,0.1);
-    padding: 2px;
-  }
-  .custom-popup .leaflet-popup-content {
-    margin: 8px 12px;
-    font-family: 'Urbanist', sans-serif;
-    min-width: 200px;
-  }
-  .custom-popup .company-name {
-    font-weight: 600;
-    color: #000000;
-    font-size: 14px;
-    margin-bottom: 4px;
-  }
-  .custom-popup .address {
-    color: #000000;
-    font-size: 12px;
-    line-height: 1.4;
-  }
-  .leaflet-popup-tip {
-    background: #FFFFFF;
-  }
-`;
+import MapComponent from "./MapFooter";
 
 const Footer = () => {
-  // Definisikan posisi dengan type yang benar
-  const position: LatLngExpression = [-6.2341214507513305, 106.92265149402358]; // Koordinat Duren Sawit
-
   return (
     <footer className="min-h-[60vh] bg-[#333333] py-8 md:py-14 px-4 md:px-16">
       <div className="flex flex-col md:flex-row">
@@ -96,30 +52,31 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="md:pl-28 pt-6 md:pt-0">
+        <div className="md:pl-28 pt-6 md:pt-0 w-[28rem]">
           <h1 className="font-urbanist font-medium text-lg text-white">
             Alamat Kantor
           </h1>
-          <p className="text-slate-300 font-light py-3">
+          <p className="text-slate-300 font-light">
             Jl. Radin Inten II No. 61A Duren Sawit
           </p>
+          <MapComponent />
+        </div>
+        <div className="md:pl-20 pt-0 ">
           <h1 className="font-urbanist font-medium text-lg text-white">
             Email
           </h1>
-          <p className="text-slate-300 font-light py-3">
+          <p className="text-slate-300 font-light pb-3">
             sales@anm.co.id <br />
             anugrah.niaga.mandiri@gmail.com
           </p>
           <h1 className="font-urbanist font-medium text-lg text-white">
             Website
           </h1>
-          <p className="text-slate-300 font-light pt-3">http://www.anm.co.id</p>
-        </div>
-        <div className="md:pl-20 pt-0 w-96">
-          <h1 className="font-urbanist font-medium text-xl text-white">
+          <p className="text-slate-300 font-light pb-3">http://www.anm.co.id</p>
+          <h1 className="font-urbanist font-medium text-lg text-white">
             Media Sosial
           </h1>
-          <div className="flex space-x-3 mt-3">
+          <div className="flex space-x-3 my-3">
             <div className="group relative cursor-pointer">
               <div className="relative rounded-lg p-1.5 border border-slate-300 group-hover:border-green-700 transition-all duration-300 transform group-hover:scale-110">
                 <svg
@@ -177,33 +134,6 @@ const Footer = () => {
                 </svg>
               </div>
             </div>
-          </div>
-          {/* Container untuk map dengan fixed height */}
-          <div className="h-[190px] w-full pt-3">
-            <style>{customPopupStyle}</style>
-            <MapContainer
-              center={position}
-              zoom={16}
-              scrollWheelZoom={false}
-              className="h-full w-full rounded-lg"
-              attributionControl={false}
-              zoomControl={true} // Menampilkan zoom control untuk navigasi yang lebih baik
-            >
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" // Kembali ke OpenStreetMap standar untuk keterbacaan yang lebih baik
-                maxZoom={19}
-              />
-              <Marker position={position} icon={redMarker}>
-                <Popup className="custom-popup">
-                  <div className="company-name">PT. Anugrah Niaga Mandiri</div>
-                  <div className="address">
-                    Jl. Radin Inten II No. 61A
-                    <br />
-                    Duren Sawit, Jakarta Timur
-                  </div>
-                </Popup>
-              </Marker>
-            </MapContainer>
           </div>
         </div>
       </div>
